714afe28740ce7b2f94ba8de3327b63bd2b9dc57
jokudev <git@joku.dev>
2024-05-10 12:53:15 +0200

    outsource navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProfileComponent } from "./components/profile/profile.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, ProfileComponent]
+    imports: [CommonModule, RouterOutlet, ProfileComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'Car Rental App (Prototype)';
