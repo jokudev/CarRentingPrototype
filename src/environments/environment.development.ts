11e11760a6a90710cf83e7b371bc122418b20793
jokudev <git@joku.dev>
2024-05-10 11:28:42 +0200

    add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://fcwufqdgibljfioeysis.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjd3VmcWRnaWJsamZpb2V5c2lzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyNjI0MTgsImV4cCI6MjAzMDgzODQxOH0.LycqSrXEGOFvrQCzxuPXkYSjR_Q7f-KX8mc6lGfb9xE',
+  },
+};
