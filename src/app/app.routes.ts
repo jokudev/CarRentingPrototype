dd5f6b7de401ad8d78fba92087f73f35dae4daa6
jokudev <git@joku.dev>
2024-05-10 11:34:59 +0200

    add the needed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SigninComponent } from './components/signin/signin.component';
+
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: '', redirectTo: 'signin', pathMatch: 'full' },
+];
